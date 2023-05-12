5fa51c6f2983bf2d5702f58c77ca664bce4e0661
williamjrr07 <114874231+williamjrr07@users.noreply.github.com>
2023-05-12 14:03:52 -0400

    modification

--- src/components/Gamescard.jsx
+++ src/components/Gamescard.jsx
@@ -17,7 +17,7 @@ const Gamescard = ({ game }) => {
             <li key={id} className="card__platform__img">
               <img
                 className="img__platform"
-                src={`../assets/images/${id}.svg`}
+                src={`./src/assets/images/${id}.svg`}
                 alt={name}
               />
             </li>
@@ -35,8 +35,7 @@ const Gamescard = ({ game }) => {
                 <a href="#">
                   <img
                     className="img__store"
-                    src={`../assets/images/${slug}.svg`}
-                    // src={`/src/assets/images/${slug}.svg`}
+                    src={`./src/assets/images/${slug}.svg`}
                     alt=""
                   />
                 </a>
